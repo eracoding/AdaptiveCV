e29f7131b937770ae04bd9a1c5a24c5092dcdf0d
Bidhan Bajracharya <95129959+Bidhan-Bajracharya@users.noreply.github.com>
2025-03-17 16:57:55 +0700

    chore: routing

--- app/components/welcome.tsx
+++ app/components/welcome.tsx
@@ -2,10 +2,10 @@ const Welcome = () => {
   return (
     <div className="bg-white/30 p-12 shadow-xl ring-1 ring-gray-900/5 rounded-lg backdrop-blur-lg max-w-xl mx-auto w-full text-black">
         <h1 className="text-4xl font-semibold text-center">
-            Welcome to the Vercel Postgres Demo
+          AI-Powered Resume Generator
         </h1>
         <p className="text-gray-700 text-center mt-4">
-            This is a demo of a Next.js app with a Postgres database deployed on Vercel.
+          Create professional resumes effortlessly with our NLP-driven builder.
         </p>
     </div>
   );
---
+++ app/pages/generate/index.tsx
@@ -0,0 +1,7 @@
+const index = () => {
+  return (
+    <div className="min-h-screen">So yea huhu</div>
+  )
+}
+
+export default index
