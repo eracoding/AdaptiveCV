56168ffd6eabf31ae140619c9dc424fe87d7f5b3
Bidhan Bajracharya <95129959+Bidhan-Bajracharya@users.noreply.github.com>
2025-03-18 19:32:50 +0700

    feat: LOGO YAY

--- app/components/wrapper/Layout.tsx
+++ app/components/wrapper/Layout.tsx
@@ -1,4 +1,5 @@
-import { Container, Flex, Group } from "@mantine/core";
+import { ActionIcon, Container, Flex, Group } from "@mantine/core";
+import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
 
@@ -23,7 +24,10 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
       {/* <header className="h-16 bg-white/30 shadow-md ring-1 ring-gray-900/5 backdrop-blur-lg"> */}
       <header className="fixed top-0 left-0 w-full h-16 bg-white/30 shadow-md ring-1 ring-gray-900/5 backdrop-blur-lg z-50">
         <Flex align={"center"} h={"100%"} px={"xl"} justify={"space-between"}>
-          <h1>LOGO</h1>
+          {/* <h1>LOGO</h1> */}
+          <ActionIcon variant="transparent" size={'xl'} onClick={() => router.push("/")} aria-label="logo">
+            <Image src="/logo.png" alt="logo" width={50} height={50} />
+          </ActionIcon>
           <Group gap="sm">
             <NavButton link={"/generate"} name={"Generate"} />
             <NavButton link={"/optimize"} name={"Optimize"} />
--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -7,6 +7,7 @@ import Welcome from "@/components/welcome";
 export default function Home() {
   return (
     <main className="relative flex min-h-screen flex-col items-center justify-center">
+      <Image src="/logo.png" alt="logo" width={60} height={60} className="absolute top-4 left-4"/>
       <Link
         href="/generate"
         className="group mt-20 sm:mt-0 rounded-full flex space-x-1 bg-white/30 shadow-sm ring-1 ring-gray-900/5 text-gray-600 text-sm font-medium px-10 py-2 hover:shadow-lg active:shadow-sm transition-all"
